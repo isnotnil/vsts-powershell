0c9037ee6e6de142900753f257d82bd0ec4fcbb1
Paul R. Bahler <paul@isnotnil.net>
2017-07-31 20:28:38 -0400

    First stab at cmdlet that gets repository commits

---
+++ Get-RepositoryCommits.ps1
@@ -0,0 +1,77 @@
+[CmdletBinding()]
+param (
+    [string]
+    $InstanceName,
+
+    [string]
+    $ProjectName,
+
+    [string]
+    $PersonalAccessToken
+)
+
+class Commit {
+    [string]$Author
+    [string]$RepositoryName
+    [string]$CommitId
+    [DateTime]$Date
+    [int]$Edits
+    [int]$Adds
+    [int]$Deletes
+}
+
+$allRepositoriesApiUrl = "https://$InstanceName.visualstudio.com/DefaultCollection/$ProjectName/_apis/git/repositories/?api-version=1.0"
+
+$vstsAuthType = "Basic"
+$vstsBasicAuthBase64String = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $vstsAuthType, $PersonalAccessToken)))
+$vstsBasicAuthHeader = "$vstsAuthType $vstsBasicAuthBase64String"
+$requestHeaders = @{Authorization = "$vstsBasicAuthHeader"}
+
+$response = Invoke-RestMethod `
+    -Method Get `
+    -Uri $allRepositoriesApiUrl `
+    -ContentType "application/json" `
+    -Headers $requestHeaders `
+    -Verbose:$VerbosePreference
+
+Write-Verbose "Found $($response.count) Repositories"
+
+$response.value | ForEach-Object {
+    $repositoryName = $_.name
+    $repositoryApiUrl = $_.url
+
+    Write-Verbose "Found Repo $($repositoryName)"
+
+    $skip = 0
+    $take = 100
+    $lastGetCount = 0
+
+    do {
+        $commitsForRepoApiUrl = "$repositoryApiUrl/commits?api-version=1.0&skip=$skip&take=$take"
+
+        $response = Invoke-RestMethod `
+            -Method Get `
+            -Uri $commitsForRepoApiUrl `
+            -ContentType "application/json" `
+            -Headers $requestHeaders `
+            -Verbose:$VerbosePreference
+
+        $lastGetCount = $response.count
+
+        $response.value | ForEach-Object {
+            $commit = [Commit]::new()
+            $commit.Author = $_.author.name
+            $commit.RepositoryName = $repositoryName
+            $commit.CommitId = $_.commitId
+            $commit.Date = $_.author.Date
+            $commit.Adds = $_.changeCounts.Add
+            $commit.Edits = $_.changeCounts.Edit
+            $commit.Deletes = $_.changeCounts.Delete
+
+            Write-Output $commit
+        }
+
+        $skip = $skip + $take
+    }
+    while ($lastGetCount -gt 0)
+}
